c0e5d12a130e8e254d306962703efa4e0daa0920
codereader <greebo@angua.at>
2024-04-19 15:08:54 +0200

    Add project files.

---
+++ CCC39/Program.cs
@@ -0,0 +1,23 @@
+﻿using CCC39Lib;
+
+var level = 1;
+
+WriteOutputs(level);
+    
+void WriteOutputs(int level)
+{
+    var solver = new Solver();
+
+    for (var inputFileNumber = 1; inputFileNumber <= 5; inputFileNumber++)
+    {
+        var inputfilename = $"../../../../Files/level{level}_{inputFileNumber}.in";
+        var lines = File.ReadAllLines(inputfilename).ToList();
+
+        var outputfilename = $"../../../../Files/level{level}_{inputFileNumber}.out";
+        using var outputWriter = new StreamWriter(outputfilename);
+
+        var output = solver.Solve(level, lines);
+
+        outputWriter.Write(output);
+    }
+}
---
+++ CCC39Lib/Solver.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace CCC39Lib;
+
+public class Solver
+{
+    public string Solve(int level, List<string> lines)
+    {
+        return level switch
+        {
+            1 => SolveLevel1(lines),
+            _ => throw new InvalidOperationException(($"Level {level} not supported."))
+        };
+    }
+
+    private string SolveLevel1(List<string> lines)
+    {
+        var actualLines = lines.Skip(1);
+        var fullResult = new StringBuilder();
+
+        foreach (var line in actualLines)
+        {
+            var counts = new[]
+            {
+                line.Count(c => c == 'W'),
+                line.Count(c => c == 'D'),
+                line.Count(c => c == 'S'),
+                line.Count(c => c == 'A'),
+            };
+
+            var resultLine = string.Join(' ', counts);
+            fullResult.AppendLine(resultLine);
+        }
+
+        return fullResult.ToString().TrimEnd('\n').TrimEnd('\r');
+    }
+}
